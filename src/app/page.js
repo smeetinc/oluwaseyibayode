dcf2681bc8a5692298e93f778cdbcfec9750ccf0
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-04-13 19:49:29 +0100

    fetch data from TMDB api

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,19 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Results from "@/components/Results";
 
+const API_KEY = process.env.API_KEY
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(  `https://api.themoviedb.org/3/${genre === "fetchTopRated"? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`, { next: {revalidate: 10000 }});
+  const data = await res.json();
+  const results = data.results;
+  
+  if(!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
   return (
-    <main>
-
-      <h1>Home</h1>    
-      
-    </main>
+    <div>
+      <Results results={results} />
+    </div>
   )
 }
