672877516f777c8839feb2d7efac3e564f203b59
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-04-13 17:05:09 +0100

    Create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in perferendis
+             numquam molestias quae nesciunt doloremque odio neque aliquid officiis minima error 
+            usto mollitia accusamus nam modi autem, voluptatum a.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in perferendis
+             numquam molestias quae nesciunt doloremque odio neque aliquid officiis minima error 
+            usto mollitia accusamus nam modi autem, voluptatum a.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in perferendis
+             numquam molestias quae nesciunt doloremque odio neque aliquid officiis minima error 
+            usto mollitia accusamus nam modi autem, voluptatum a.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in perferendis
+             numquam molestias quae nesciunt doloremque odio neque aliquid officiis minima error 
+            usto mollitia accusamus nam modi autem, voluptatum a.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in perferendis
+             numquam molestias quae nesciunt doloremque odio neque aliquid officiis minima error 
+            usto mollitia accusamus nam modi autem, voluptatum a.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in perferendis
+             numquam molestias quae nesciunt doloremque odio neque aliquid officiis minima error 
+            usto mollitia accusamus nam modi autem, voluptatum a.</p>
+            
+    </div>
+  )
+}
