eedf99f503af202d578c3101ea35f0dd94b9038a
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-04-16 15:46:21 +0100

    Add search ability

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import Providers from './Providers'
 import Navbar from '@/components/Navbar'
+import SearchBox from '@/components/searchBox'
 
 export const metadata = {
   title: 'Oluwaseyi',
@@ -23,6 +24,8 @@ export default function RootLayout({ children }) {
         <Navbar />
 
         {/* SearchBox */}
+
+        <SearchBox />
         
         
         
